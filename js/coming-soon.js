353897190f26b6d6bf3e5d1b5f351cf6b32e94ff
Thiago Salles <thiagosalles@MacBook-Pro.local>
2019-01-19 21:53:56 -0200

    Reseting the form using js

--- js/coming-soon.js
+++ js/coming-soon.js
@@ -1,6 +1,9 @@
 (function($) {
   "use strict"; // Start of use strict
 
-  // No JS
+  $('#myform').ajaxForm(function() {
+    alert('Seu interesse foi registrado. Em breve entraremos em contato');
+    $('#myform').trigger('reset')
+  })
 
 })(jQuery); // End of use strict
