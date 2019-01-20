115f482360f99ff8607e87b7d860f28b1c976e8d
Thiago Salles <thiagosalles@devhat.com.br>
2019-01-19 23:13:42 -0200

    Including missing licence information

--- js/coming-soon.js
+++ js/coming-soon.js
@@ -1,3 +1,9 @@
+/*!
+ * Start Bootstrap - Coming Soon v5.0.1 (https://startbootstrap.com/template-overviews/coming-soon)
+ * Copyright 2013-2019 Start Bootstrap
+ * Licensed under MIT (https://github.com/BlackrockDigital/startbootstrap-coming-soon/blob/master/LICENSE)
+ */
+
 (function($) {
   "use strict"; // Start of use strict
 
